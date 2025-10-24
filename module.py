503d04e6510765207f457fa9d9bff7fd2a8e1271
Wilona2 <2172985@cegepoutaouais.qc.ca>
2025-10-24 16:03:30 -0400

    Terminé pseudocode Corrections à la fonction generer_carte

--- module.py
+++ module.py
@@ -33,29 +33,21 @@ def generer_carte(liste: list) -> list:
         chiffre += 1
 
     #Séparer la liste de chiffres en sections selon leur lettre correspondante
-    chiffres_b = liste_chiffres[0:15] #ex: seuls les chiffres 1 à 15 se retrouvent sous la lettre 'B'
+    chiffres_b = liste_chiffres[0:15] #exemple: seuls les chiffres 1 à 15 se retrouvent sous la lettre 'B'
     chiffres_i = liste_chiffres[15:30]
-    chiffres_n = liste_chiffres[31:45]
+    chiffres_n = liste_chiffres[30:45]
     chiffres_g = liste_chiffres[45:60]
     chiffres_o = liste_chiffres[60:]
     liste_chiffres = [chiffres_b, chiffres_i, chiffres_n, chiffres_g, chiffres_o]
 
     #choisir les chiffres aléatoirement et les ajouter dans la liste carte_joueur
-    index = 1
     for i in range(len(liste_chiffres)):
         for j in range(len(liste_chiffres)):
             choix_random = random.choice(liste_chiffres[j])
-            carte_joueur[index].append(choix_random)
+            carte_joueur[i+1].append(choix_random)
             liste_chiffres[j].remove(choix_random)
-        index += 1 #passer à la prochaine ligne de la carte joueur
 
     #Changer la case du milieu pour un espace libre
     carte_joueur[3].pop(2)
     carte_joueur[3].insert(2, "*")
     return carte_joueur
-
-#test
-liste_combinaisons = creer_liste_combinaisons()
-carte_j = generer_carte(liste_combinaisons)
-for ligne in carte_j:
-    print(ligne)
--- programme_principal.py
+++ programme_principal.py
@@ -17,47 +17,106 @@ Fonction qui détermine si on a une ligne BINGO gagnante
 Fonction qui place un jeton sur la carte BINGO
 
 """
-from random import randrange
-
 """
 Pseudocode :
+
+##Fonctions:
+creer_liste_combinaisons() -> list:
+    Fonction qui crée une liste avec toutes les combinaisons lettres-chiffres possibles à piger dans un
+    jeu de BINGO.
+    :return: liste des combinaisons
+    ----------------------------------
+    Liste lettres = ["B", "I", "N", "G", "O"]
+    Liste combinaisons = [] 
+    
+    Chiffre = 0
+    Index de la liste lettre = 0
+    Boucle for (pour chaque lettre dans liste lettres):
+        Lettre = liste lettres(index de la liste lettre)
+        Boucle for (15 fois):
+            Chiffre +=1 (le chiffre 0 n'est pas inclu dans ce jeu de bingo)
+            Ajouter lettre + chiffre à la liste de combinaisons
+        Index de la liste lettre +=1
+    Return liste combinaisons
+
+generer_carte(liste: list) -> list:
+    Fonction qui génère une carte BINGO aléatoire pour le joueur
+    :param liste: liste de combinaisons
+    :return: une liste représentant la carte du joueur
+    --------------------------------------------------
+    Carte joueur (vide) = [["B", "I", "N", "G", "O" ], [], [], [], [], []]
+    
+    Avec une boucle for, creer une liste contenant tous les chiffres possibles dans un jeu de BINGO.
+    
+    Avec des split, séparer la liste de chiffres en sections selon leur lettre correspondante.
+    (B(1-15), I(16-30), N(31-45), G(46-60), O(61-75)) Note: quand on fait un split, le premier chiffre n'est pas inclus.
+    Liste chiffres = liste 2D contenant chaque section de chiffres par lettre
+    
+    Choisir les chiffres aléatoirement et les ajouter dans la liste carte joueur
+    Boucle for i in range(pour chaque lettre):
+        Boucle for j in range(pour chaque lettre): 
+            choix chiffre = random.choice(liste chiffres[j])
+            Ajouter chiffre à la carte joueur[i+1]
+            Retirer chiffre de la liste chiffres
+    
+    Changer la case du milieu pour un espace libre
+    Return la carte joueur
+            
+
+placer_jeton(combi_pigee: str, carte: list[list[str and int]]) -> list[list[str and int]]:
+    Fonction place un jeton sur la carte BINGO
+    :param combi_pigee: la combinaison lettre-chiffre pigée
+    :param carte: la carte BINGO du joueur
+    :return: la carte bingo du joueur avec modification (jeton)
+    ---------------------------------------
+    Demander au joueur de placer son jeton au bon endroit (choisir la bonne colonne et la bonne ligne)
+    Si combi_pigee n'existe pas dans la carte du joueur, le joueur doit répondre avec 'x' (ou autre symbole)
+    Si le joueur ne choisit pas la bonne réponse, on lance un message d'erreur 
+    Si le joueur choisit le bon endroit, on remplace le chiffre assigné à cette case par un jeton 
+    (o, *, peu importe tant que ce n'est pas un chiffre)
+    Return la carte bingo du joueur avec son nouveau jeton (ou inchangée si la combinaison n'existe pas 
+    dans la carte du joueur) 
+
+bingo_gagnant(c_joueur: list[list[str and int]]) -> str:
+    Fonction qui détermine si on a une ligne BINGO gagnante
+    :param c_joueur: la carte BINGO du joueur
+    :return: "BINGO gagnant!" ou "Pas encore de BINGO."
+    ------------------------------------------
+    gagné = False
+    Pour chaque colonne de la liste c_joueur:
+        Si les indexes de la colonne sont tous des string, 
+            gagné = True
+    Pour chaque ligne de la liste c_joueur:
+        Si les indexes de la ligne sont tous des string, 
+            gagné = True
+    Pour chaque ligne diagonale dans la liste c_joueur:
+        Si les indexes de la ligne diagonale sont tous des string, 
+            gagné = True
+    
+    Si gagné == True:
+        return "BINGO gagnant!"
+    Sinon:
+        return "Pas encore de BINGO."
+
+##Code Principal: 
 importer random 
 
-Carte bingo du joueur = fonction qui génère une carte unique pour le joueur 
-
+Liste combinaisons = fonction creer_liste_combinaisons()
+Carte bingo du joueur = fonction generer_carte(liste combinaisons)
+    
 Boucle while :
-    Combinaison lettre-chiffre = combinaison random choisie à partir de la liste de combinaisons (ex: B-12, I-18, ...)
+    Combinaison lettre-chiffre = random.choice(liste combinaisons)
     Afficher la carte BINGO du joueur
     Afficher la combinaison lettre-chiffre pigée
     
-    Utiliser la fonction placer_jeton(combinaison_pigee: str, carte_joueur: list[list[str and int]]) -> list[list[str and int]]:
-        Demander au joueur de placer son jeton au bon endroit (choisir la bonne colonne et la bonne ligne)
-        Si la combinaison n'existe pas dans la carte du joueur, le joueur doit répondre avec 'x' (ou autre symbole)
-        Si le joueur ne choisit pas la bonne réponse, on lance un message d'erreur 
-        Si le joueur choisit le bon endroit, on remplace le chiffre assigné à cette case par un jeton 
-        (o, *, peu importe)
-        Return la carte bingo du joueur avec son nouveau jeton (ou inchangée si la combinaison n'existe pas 
-        dans la carte du joueur) 
+    Utiliser la fonction placer_jeton(combinaison_pigee, carte_joueur)
     
-    Utiliser la fonction bingo_gagnant(carte_joueur: list[list[str and int]]) -> str:
-        Pour chaque colonne de la liste carte_joueur:
-            Si indexe y == str, 
-                return "bingo gangnant"
-        Pour chaque ligne de la liste carte_joueur:
-            Si indexe y == str, 
-                return "bingo gangnant"
-        Pour chaque ligne diagonale dans la liste carte_joueur:
-            Si indexe y == str, 
-                return "bingo gangnant"
-    ^^^^^^^^ à terminer plus tard
+    Utiliser la fonction bingo_gagnant(carte_joueur)
     
-    if bingo = "bingo gangnant":
-        break    
-    
-    
-    
-  
+    if bingo == "BINGO gagnant!":
+        break 
 """
+from random import randrange
 
 from module import *
 import random
@@ -120,39 +179,6 @@ def bingo_gagnant(grille):
 
 #Fonction placer le jeton
 ####
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
